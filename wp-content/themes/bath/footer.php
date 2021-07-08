0c3fee86e51a08f322f519ee724b8277b08fd5e3
Volodimir Ilinskiy <creavit@i.ua>
2021-07-08 17:10:46 +0300

    ADDED | footer social icons

--- wp-content/themes/bath/footer.php
+++ wp-content/themes/bath/footer.php
@@ -1,19 +1,24 @@
-
 <footer>
   <div class="container">
     <div class="row">
-      <div class="col s3">
-        <div class="footer-logo">
-          <img src="<?php echo THEME_DIR_URI.'/dist/images/'?>QA-Logo-Navy.png">
+      <div class="col s12">
+        <div class="valign-wrapper">
+          <div class="footer-logo">
+            <img src="<?php echo THEME_DIR_URI.'/dist/images/'?>QA-Logo-Navy.png">
+          </div>
+          <div class="social-icons">
+            <?php 
+              $networks = get_field('social_networks', 'options');
+              foreach($networks as $network){
+                echo '<a href="'.$network['link_network'].'" target="_blank"><i class="fa fa-'.$network['icon'].'" aria-hidden="true"></i></a>';
+              }
+            ?>
+          </div>
         </div>
       </div>
-      <div class="social">F</div>
-      
     </div>
   </div>
 </footer>
-
-<?php wp_footer(); ?>
-    
+<?php wp_footer(); ?>   
 </body>
 </html>
